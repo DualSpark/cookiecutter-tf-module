6dc55f2b7091506e96e34fbf6282e7c1c746b65c
Patrick McClory <pmcclory@datapipe.com>
2015-09-30 14:08:56 -0700

    updating again

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals, absolute_import, print_function
 import os
 import shutil
 from collections import OrderedDict
-from cookiecutter.prompt import query_yes_no
+from cookiecutter.prompt import read_user_yes_no
 
 try:
     input = raw_input
@@ -14,7 +14,7 @@ except NameError:
 components = OrderedDict()
 
 components['awsconfig'] = {
-    'question': '\nShould it include AWS configuration variable declarations? ',
+    'question': '\nInclude AWS configuration variable declarations? ',
     'hint': '  Include AWS access and secret key variables ',
     'filename': 'variables.tf',
     'append': '\n// Variables for AWS Provider\nvariable "aws_access_key" {}\nvariable "aws_secret_key" {}\nvariable "aws_region" {}'
@@ -23,7 +23,7 @@ components['awsconfig'] = {
 def configure_role():
     print('\n\nMODULE CONFIGURATION:\n=====================')
     for action_name, action in components.items():
-        if query_yes_no(components[action['question']]):
+        if read_user_yes_no(components[action['question']], 'Yes'):
             if 'fiename' in action:
                 if 'append' in action:
                     with open(action['filename'], 'a') as f:
